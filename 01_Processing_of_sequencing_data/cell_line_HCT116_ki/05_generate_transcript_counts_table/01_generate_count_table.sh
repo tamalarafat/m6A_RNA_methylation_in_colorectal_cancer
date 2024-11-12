d97d730b5db34efffd0954d64ddce3127e18f2d1
Yasir Arafat Tamal <ya_tamal@macbookpro.speedport.ip>
2024-11-12 15:39:48 +0100

    Adding project findings and report

---
+++ 01_Processing_of_sequencing_data/cell_line_HCT116_ki/05_generate_transcript_counts_table/01_generate_count_table.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Directory containing the indexed basecalled reads
+data_dir="/ds/storage/common/ytamal_work/Project_14052024_ONT_RNAseq/PAS98400_FLO_PRO004RA_SQK_RNA004_HCT_Ki/analysis_of_the_sample/analysis_output/basecalled_reads_sorted.bam"
+
+# Output directory of the indexed bam file
+output_dir="/ds/storage/common/ytamal_work/Project_14052024_ONT_RNAseq/PAS98400_FLO_PRO004RA_SQK_RNA004_HCT_Ki/analysis_of_the_sample/analysis_output"
+
+# Create output directory if it doesn't exist
+mkdir -p "$output_dir"
+
+# Convert all pod5 files to fast5 files
+time NanoCount -i "$data_dir" --extra_tx_info -o "$output_dir/hct_ki.tsv" --verbose
